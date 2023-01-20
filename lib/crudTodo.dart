eaa27e47e5ac56712dc9e46b3dc34685206cc637
HirayamaSachi <hirayamas0305@gmail.com>
2023-01-21 01:15:32 +0900

    状態管理動くようになった

--- lib/crudTodo.dart
+++ lib/crudTodo.dart
@@ -10,23 +10,21 @@ class TodoData extends InheritedWidget {
     this.data,
     required this.todo,
     required this.child,
-  }) :assert(child!=null) ,super(key: key, child: child);
+  })  : assert(child != null),
+        super(key: key, child: child);
 
-  static TodoData? maybeOf(BuildContext context) {
-    return context.dependOnInheritedWidgetOfExactType<TodoData>();
-  }
-   static TodoData of(BuildContext context) {
-    final TodoData? result = maybeOf(context);
-    assert(result != null, 'No TodoData found in context');
-    return result!;
-  }
   @override
-  bool updateShouldNotify(TodoData oldWidget) => todo != oldWidget.todo;
+  bool updateShouldNotify(covariant InheritedWidget oldWidget) => true;
 }
 
 class TodoManager extends StatefulWidget {
   const TodoManager({Key? key, required this.child}) : super(key: key);
   final Widget child;
+
+  static TodoManagerState? of(BuildContext context) {
+    return context.dependOnInheritedWidgetOfExactType<TodoData>()!.data;
+  }
+
   @override
   State<TodoManager> createState() => TodoManagerState();
 }
@@ -71,6 +69,6 @@ class TodoManagerState extends State<TodoManager> {
 
   @override
   Widget build(BuildContext context) {
-    return TodoData(todo: todo, data: this,child: Builder(builder: ((context) => widget.child )));
+    return TodoData(todo: todo, data: this, child: widget.child);
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -7,48 +7,33 @@ import 'crudTodo.dart';
 void main() {
   runApp(const MyApp());
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return TodoManager(child: MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
+      home: Scaffold(
       appBar: AppBar(),
-      body: Column(
-        children: const [
-          TodoManager(child: ShowTodo(),
-          ),
-        ],
+      body: 
+          const TodoManager(child:  ShowTodo()),
       ),
-    );
+    ),);
   }
 }
 
 class ShowTodo extends StatelessWidget {
   const ShowTodo({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    final todo = TodoData.of(context).todo;
+    final todo = TodoManager.of(context)?.todo;
     return ListView.builder(
-        itemCount: todo.length,
+        itemCount: todo?.length,
         shrinkWrap: true,
         physics: NeverScrollableScrollPhysics(),
         itemBuilder: (BuildContext context, int index) {
@@ -61,17 +46,17 @@ class ShowTodo extends StatelessWidget {
                 color: Colors.red,
                 // delete処理
                 onTap: () {
-                  TodoData.of(context).data?.delete(index);
+                  TodoManager.of(context)?.delete(index);
                 },
               )
             ],
             // child: CmpTodo(index),
             child: CheckboxListTile(
-                title: Text(todo[index]['name']),
-                value: todo[index]['completed'] ? true : false,
+                title: Text(todo?[index]['name']),
+                value: todo?[index]['completed'] ? true : false,
                 // 完了処理
                 onChanged: ((value) {
-                  TodoData.of(context).data?.completeToggle(index);
+                  TodoManager.of(context)?.completeToggle(index);
                 }),
                 controlAffinity: ListTileControlAffinity.leading,
                 secondary: IconButton(
@@ -81,62 +66,5 @@ class ShowTodo extends StatelessWidget {
                     ))),
           );
         });
-  }
-}
-
-class AddTodo extends StatelessWidget {
-  const AddTodo({required this.todoList, required this.onChanged, Key? key})
-      : super(key: key);
-
-  final List<Map<String, dynamic>> todoList;
-  final ValueChanged<String> onChanged;
-
-  @override
-  Widget build(BuildContext context) {
-    return FloatingActionButton(
-      child: Icon(Icons.add),
-      onPressed: () {
-        // ダイアログを表示
-        insertTodo(context);
-      },
-    );
-  }
-
-  // コールバック
-  void _addTodo(value) {
-    onChanged(value);
-  }
-
-  void insertTodo(BuildContext context) {
-    String _text = "";
-    // todoに入れるための一時的な変数
-    void _handleText(String value) {
-      _text = value;
-    }
-
-    showDialog(
-        context: context,
-        builder: ((context) {
-          final TextEditingController _controller = TextEditingController();
-          return StatefulBuilder(builder: (context, StateSetter setState) {
-            return AlertDialog(
-              title: Text('タスク追加'),
-              content: TextField(
-                controller: _controller,
-                onChanged: _handleText,
-              ),
-              actions: [
-                ElevatedButton(
-                  onPressed: () {
-                    (_text != "") ? _addTodo(_text) : null;
-                    // ダイアログを抜ける
-                    Navigator.pop(context);
-                  },
-                  child: Text('追加'),
-                )
-              ],
-            );
-          });
-        }));
   }
 }
