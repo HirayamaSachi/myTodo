5b1422030446c8bc31696334d5a9667af5febb89
HirayamaSachi <hirayamas0305@gmail.com>
2023-02-08 01:09:03 +0900

    add:タスクのフィルタリングボタン

--- lib/main.dart
+++ lib/main.dart
@@ -35,61 +35,71 @@ class ShowTodo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final todo = TodoManager.of(context).todo;
-    return Padding(
-      padding: const EdgeInsets.all(12.0),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Expanded(
-            child: ListView.builder(
-                itemCount: todo.length,
-                shrinkWrap: true,
-                itemBuilder: (BuildContext context, int index) {
-                  return Slidable(
-                    actionPane: SlidableDrawerActionPane(),
-                    secondaryActions: [
-                      IconSlideAction(
-                        caption: '削除',
-                        icon: Icons.delete,
-                        color: Colors.red,
-                        // delete処理
-                        onTap: () {
-                          TodoManager.of(context, rebuild: false).delete(index);
-                        },
-                      )
-                    ],
-                    // child: CmpTodo(index),
-                    child: CheckboxListTile(
-                        title: Text(todo[index].name),
-                        value: todo[index].completed ? true : false,
-                        // 完了処理
-                        onChanged: ((value) {
-                          TodoManager.of(context, rebuild: false)
-                              .completeToggle(index);
-                        }),
-                        controlAffinity: ListTileControlAffinity.leading,
-                        secondary: IconButton(
-                            onPressed: (() async{
-                              var result= await dialogBuilder(context, isNew: false);
-                              if(result == null || result == ""){return;}
-                              TodoManager.of(context).update(index, result);
-                            }),
-                            icon: const Icon(
-                              Icons.create,
-                            ))),
-                  );
-                }),
-          ),
-          FloatingActionButton(
-            onPressed: (() async {
-              var result = await dialogBuilder(context, isNew: true);
-              if(result == null || result == ""){return;}
-              TodoManager.of(context, rebuild: false).create(result.toString());
-            }),
-            child: Icon(Icons.add),
-          )
-        ],
-      ),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+          TextButton(child: const Text('All'), onPressed: (() {
+            // TODO: filterかける
+          })),
+          TextButton(child: const Text('Active'), onPressed: (() {
+            // TODO: filterかける
+          })),
+          TextButton(child: const Text('Completed'), onPressed: (() {
+            // TODO: filterかける
+          }))
+        ],),
+        Expanded(
+          child: ListView.builder(
+              itemCount: todo.length,
+              shrinkWrap: true,
+              itemBuilder: (BuildContext context, int index) {
+                return Slidable(
+                  actionPane: SlidableDrawerActionPane(),
+                  secondaryActions: [
+                    IconSlideAction(
+                      caption: '削除',
+                      icon: Icons.delete,
+                      color: Colors.red,
+                      // delete処理
+                      onTap: () {
+                        TodoManager.of(context, rebuild: false).delete(index);
+                      },
+                    )
+                  ],
+                  // child: CmpTodo(index),
+                  child: CheckboxListTile(
+                      title: Text(todo[index].name),
+                      value: todo[index].completed ? true : false,
+                      // 完了処理
+                      onChanged: ((value) {
+                        TodoManager.of(context, rebuild: false)
+                            .completeToggle(index);
+                      }),
+                      controlAffinity: ListTileControlAffinity.leading,
+                      secondary: IconButton(
+                          onPressed: (() async{
+                            var result= await dialogBuilder(context, isNew: false);
+                            if(result == null || result == ""){return;}
+                            TodoManager.of(context).update(index, result);
+                          }),
+                          icon: const Icon(
+                            Icons.create,
+                          ))),
+                );
+              }),
+        ),
+        FloatingActionButton(
+          onPressed: (() async {
+            var result = await dialogBuilder(context, isNew: true);
+            if(result == null || result == ""){return;}
+            TodoManager.of(context, rebuild: false).create(result.toString());
+          }),
+          child: Icon(Icons.add),
+        )
+      ],
     );
   }
 }
